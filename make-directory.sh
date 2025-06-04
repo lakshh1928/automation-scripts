c53e7eda81a886978b14b4cd1d43788a288a100f
lakshya <cont.lakshya@gmail.com>
2025-06-04 17:37:14 +0530

    Made it for making directory

---
+++ make-directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+read -p "Enter the Directory name: " dir
+
+mkdir $dir
--- make-file.sh
+++ make-file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -p "Enter the file name" namme
-read -p "Enter the file extension" ex
+read -p "Enter the file name: " namme
+read -p "Enter the file extension: " ex
 
 touch $namme.$ex
 
